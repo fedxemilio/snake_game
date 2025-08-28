e66f4065b018b7319d9a8998c7cc65f81f5c989a
fedxemilio <fedxemilio@gmail.com>
2025-08-28 16:05:03 -0600

    how to play menus

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ from objects import Negg, Bomb
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Meerca Chase Clone - Movement, Tail & Neggs")
+pygame.display.set_caption("Snake Game")
 clock = pygame.time.Clock()
 
 class Game:
@@ -44,6 +44,7 @@ class Game:
         self.draw_text("The Snake Game", WIDTH // 2, HEIGHT * 1/4, WHITE, size=48)
 
         start_btn = self.draw_text("Start Game", WIDTH // 2, HEIGHT // 2, WHITE)
+        help_btn = self.draw_text("How to Play", WIDTH // 2, HEIGHT * 3/5, WHITE)
         settings_btn = self.draw_text("Settings", WIDTH // 2, HEIGHT * 2/3, WHITE)
         quit_btn = self.draw_text("Quit", WIDTH // 2, HEIGHT * 3/4, WHITE)
 
@@ -55,12 +56,78 @@ class Game:
         if start_btn.collidepoint(mouse) and clicked:
             self.state = 'playing'
             return 'playing'
+        elif help_btn.collidepoint(mouse) and clicked:
+            self.state = 'help'
+            return 'help'
         elif settings_btn.collidepoint(mouse) and clicked:
             self.state = 'settings'
             return 'settings'
         elif quit_btn.collidepoint(mouse) and clicked:
             pygame.quit()
             sys.exit()
+
+    def help_menu(self):
+        screen.fill(WHITE)
+        self.draw_text("How To Play", WIDTH // 2, HEIGHT//20, BLACK, size=64)
+        text = ["The snake must go around collecting neggs all while avoiding hitting its",
+        "own tail (or the bombs). Press the LEFT button to make a left turn and",
+        "RIGHT button to make a right turn. Neggs will increase your snake's tail",
+        "and earn points according to their color. Powerups will also appear to help!",
+        "Press the H key while playing to view this menu."]
+        padding = HEIGHT // 20
+        for i in range(len(text)):
+            self.draw_text(text[i], WIDTH//50, (HEIGHT * 1/5 + padding*i), BLACK, size=24, centered=False)
+        self.draw_text("Negg Types", WIDTH//10, HEIGHT//2, BLACK, centered=False)
+        y_padding = 10
+        for i in range(len(NEGG_TYPES)):
+            y = (HEIGHT * 3/5) + (block_size + y_padding) * i
+            pygame.draw.rect(screen, NEGG_TYPES[i].get('color'), (WIDTH//10, y, block_size, block_size))
+            self.draw_text(f"{NEGG_TYPES[i].get('points')}pts", WIDTH//10 + block_size + y_padding, y, BLACK, size=16, centered=False)
+        self.draw_text("Powerup Types", WIDTH * 3/5, HEIGHT//2, BLACK, centered=False)
+        for i in range(len(BONUS_NEGGS)):
+            y = (HEIGHT * 3/5) + (block_size + y_padding) * i
+            pygame.draw.rect(screen, BONUS_NEGGS[i].get('color'), (WIDTH * 3/5, y, block_size, block_size))
+            self.draw_text(BONUS_NEGGS[i].get('description'), WIDTH*3/5 + block_size + y_padding, y, BLACK, size=16, centered=False)
+
+        back_btn = self.draw_text("Back", WIDTH*9/10, HEIGHT//20, BLACK)
+        events = pygame.event.get()
+        mouse = pygame.mouse.get_pos()
+        clicked = any(event.type == pygame.MOUSEBUTTONDOWN for event in events)
+
+        if back_btn.collidepoint(mouse) and clicked:
+            self.state = 'menu'
+            return 'menu'
+
+        pygame.display.flip()
+
+    def get_help_menu(self):
+        while True:
+            pan_width, pan_height = WIDTH*4/5, HEIGHT*4/5
+            panel = pygame.Surface((pan_width, pan_height))
+            panel.fill(BLACK)
+            screen.blit(panel, (WIDTH//2 - pan_width//2, HEIGHT//2 - pan_height//2))
+
+            self.draw_text("Negg Types", WIDTH//8, HEIGHT*1/5, WHITE, centered=False)
+            y_padding = 10
+            for i in range(len(NEGG_TYPES)):
+                y = (HEIGHT * 2/5) + (block_size + y_padding) * i
+                pygame.draw.rect(screen, NEGG_TYPES[i].get('color'), (WIDTH//8, y, block_size, block_size))
+                self.draw_text(f"{NEGG_TYPES[i].get('points')}pts", WIDTH//8 + block_size + y_padding, y, WHITE, size=16, centered=False)
+            self.draw_text("Powerup Types", WIDTH * 2/5, HEIGHT*1/5, WHITE, centered=False)
+            for i in range(len(BONUS_NEGGS)):
+                y = (HEIGHT * 2/5) + (block_size + y_padding) * i
+                pygame.draw.rect(screen, BONUS_NEGGS[i].get('color'), (WIDTH * 2/5, y, block_size, block_size))
+                self.draw_text(BONUS_NEGGS[i].get('description'), WIDTH*2/5 + block_size + y_padding, y, WHITE, size=16, centered=False)
+
+            pygame.display.flip()
+            
+            back_btn = self.draw_text("Back", WIDTH*9/10, HEIGHT//20, BLACK)
+            events = pygame.event.get()
+            mouse = pygame.mouse.get_pos()
+            clicked = any(event.type == pygame.MOUSEBUTTONDOWN for event in events)
+
+            if back_btn.collidepoint(mouse) and clicked:
+                break
 
     def settings(self):
         events = pygame.event.get()
@@ -97,6 +164,8 @@ class Game:
                     self.player.direction = left_turn[self.player.direction] #change direction
                 elif event.key == pygame.K_RIGHT:
                     self.player.direction = right_turn[self.player.direction]
+                elif event.key == pygame.K_h:
+                    self.get_help_menu()
 
         self.player.update()
         #update tail
